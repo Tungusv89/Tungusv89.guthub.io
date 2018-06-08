ea963edc63abd2b1d2af6bb9011dd332b0686af6
Evgeny <tungusv89@mail.ru>
2018-06-09 01:18:00 +0500

    Первый комит

---
+++ script.js
@@ -0,0 +1,23 @@
+//Back to top
+var $btnTop = $('.btn-top');
+$(window).on("scroll",function (){
+	if($(window).scrollTop() >= 400){
+		$btnTop.fadeIn();
+	} else{
+		$btnTop.fadeOut();
+	}
+});
+
+$btnTop.on("click", function () {
+	$("html,body").animate({scrollTop:0}, 500)
+});
+// Скрол к якорям
+$(document).ready(function() {
+   $("a.scrollto").click(function () {
+   	elementClick = $(this).attr("href")
+   	destination = $(elementClick).offset().top;
+   	$("html:not(:animated),body:not(:animated)").animate({scrollTop:destination}, 1000);
+   })
+     return false;
+   });
+ 
